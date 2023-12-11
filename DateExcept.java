3f97fce4bcbab991304d0e7382606101be37bf8b
Daniil <120733028+dkon0v@users.noreply.github.com>
2023-12-11 17:08:50 +0300

    Add files via upload

---
+++ DateExcept.java
@@ -0,0 +1,10 @@
+public class DateExcept extends Exception {
+    public DateExcept() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DateNotCorrectFormat");
+        System.out.printf("It`s not correct date format: %s", i);
+        System.out.println();
+    }
+}
